e7a2b55e42f52d9146b6f21abb375c61ee419aef
BangGao <greenbacyon@gmail.com>
2022-02-03 22:01:08 +0000

    origin version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class GameManager :MonoBehaviour
+{
+    private bool judge;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
